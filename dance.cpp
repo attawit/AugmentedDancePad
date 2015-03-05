80c72619fee656990a7afebefa63315eaf29225c
Mengyuan Gu <gmy.melissa@gmail.com>
2015-03-05 12:23:50 -0800

    init

---
+++ dance.cpp
@@ -0,0 +1,222 @@
+// Skeleton Code for CS290I Homework 1
+// 2012, Jon Ventura and Chris Sweeney
+
+// adapt the include statements for your system:
+
+#include "background_segm.hpp"
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <opencv2/opencv.hpp>
+
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glut.h>
+#endif
+
+#include <cstdio>
+#include <fstream>
+
+
+using namespace cv;
+using namespace std;
+
+cv::VideoCapture *cap = NULL;
+int width = 1280;
+int height = 720;
+Mat image;
+
+
+
+// a useful function for displaying your coordinate system
+void drawAxes(float length)
+{
+    glPushAttrib(GL_POLYGON_BIT | GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT) ;
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE) ;
+    glDisable(GL_LIGHTING) ;
+    
+    glBegin(GL_LINES) ;
+    glColor3f(1,0,0) ;
+    glVertex3f(0,0,0) ;
+    glVertex3f(length,0,0);
+    
+    glColor3f(0,1,0) ;
+    glVertex3f(0,0,0) ;
+    glVertex3f(0,length,0);
+    
+    glColor3f(0,0,1) ;
+    glVertex3f(0,0,0) ;
+    glVertex3f(0,0,length);
+    glEnd() ;
+    
+    
+    glPopAttrib() ;
+}
+
+
+
+
+void display()
+{
+    // clear the window
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    cv::Mat tempimage;
+    flip(image, tempimage, 0);
+
+    
+    
+    glDisable(GL_DEPTH_TEST);
+    glDrawPixels( tempimage.size().width, tempimage.size().height, GL_BGR, GL_UNSIGNED_BYTE, tempimage.ptr() );
+    glEnable(GL_DEPTH_TEST);
+    
+    
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    // Here, set up new parameters to render a scene viewed from the camera.
+    
+    //set viewport
+    glViewport(0, 0, tempimage.size().width, tempimage.size().height);
+    
+    //set projection matrix using intrinsic camera params
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    
+    //you will have to set modelview matrix using extrinsic camera params
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    
+    
+    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+    
+    
+    /////////////////////////////////////////////////////////////////////////////////
+    // Drawing routine
+    
+    //now that the camera params have been set, draw your 3D shapes
+    //first, save the current matrix
+   
+    
+    glTranslatef(0, 0, 0);
+    //drawAxes(1.0);
+    
+    
+    // show the rendering on the screen
+    glutSwapBuffers();
+    
+    // post the next redisplay
+    glutPostRedisplay();
+}
+
+void reshape( int w, int h )
+{
+    // set OpenGL viewport (drawable area)
+    glViewport( 0, 0, w, h );
+}
+
+void mouse( int button, int state, int x, int y )
+{
+    if ( button == GLUT_LEFT_BUTTON && state == GLUT_UP )
+    {
+        
+    }
+}
+
+void keyboard( unsigned char key, int x, int y )
+{
+    switch ( key )
+    {
+        case 'q':
+            // quit when q is pressed
+            exit(0);
+            break;
+        default:
+            break;
+    }
+}
+
+void idle()
+{
+    // grab a frame from the camera
+    (*cap) >> image;
+    
+    if (image.empty()) {
+        cout << "No captured frame" << endl;
+        exit(0);
+    }
+}
+
+
+
+int main( int argc, char **argv )
+{
+    int w,h;
+    
+    if ( argc == 1 ) {
+        // start video capture from camera
+        cap = new cv::VideoCapture(0);
+    } else if ( argc == 2 ) {
+        // start video capture from file
+        cap = new cv::VideoCapture(argv[1]);
+    } else {
+        fprintf( stderr, "usage: %s [<filename>]\n", argv[0] );
+        return 1;
+    }
+    
+    // check that video is opened
+    if ( cap == NULL || !cap->isOpened() ) {
+        fprintf( stderr, "could not start video capture\n" );
+        return 1;
+    }
+    
+    // get width and height
+    w = (int) cap->get( CV_CAP_PROP_FRAME_WIDTH );
+    h = (int) cap->get( CV_CAP_PROP_FRAME_HEIGHT );
+    // On Linux, there is currently a bug in OpenCV that returns
+    // zero for both width and height here (at least for video from file)
+    // hence the following override to global variable defaults:
+    width = w ? w : width;
+    height = h ? h : height;
+
+    
+    // initialize GLUT
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_RGBA| GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowPosition( 20, 20 );
+    glutInitWindowSize( width, height );
+
+    glutCreateWindow( "Augmented Dance Pad" );
+    
+    
+    // set up GUI callback functions
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    glDepthFunc(GL_LESS);
+    glShadeModel(GL_SMOOTH);
+    glDisable(GL_CULL_FACE); // Nicer looking teapots.
+    glClearColor(0.0f, 0.0f, 0.0f, 1);
+    glEnable(GL_DEPTH_TEST);
+    glClearDepth(1.0f);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glDisable(GL_COLOR_MATERIAL);
+    
+    
+    
+    
+
+    glutDisplayFunc( display );
+    glutReshapeFunc( reshape );
+    glutMouseFunc( mouse );
+    glutKeyboardFunc( keyboard );
+    glutIdleFunc( idle );
+    
+    // start GUI loop
+    glutMainLoop();
+    
+    return 0;
+}
